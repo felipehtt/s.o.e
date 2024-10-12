b8f2ce9d8582518913f2329d20faf3ff7fe87f16
davi <daviforever17@gmail>
2024-10-12 20:11:34 -0300

    not found.

--- src/pages/naoencontrado/index.jsx
+++ src/pages/naoencontrado/index.jsx
@@ -1,17 +1,33 @@
 import './index.scss';
-
-import Cabecalho from '../../components/cabecalho';
-import Footer from '../../components/footer';
+import { useNavigate } from 'react-router-dom';
 
 export default function NaoEncontrado() {
 
+    const navigate = useNavigate();
+
+    function voltar(){
+
+        navigate('/');
+
+    }
+
+
+
     return (
 
-        <div className='pagina-naoencontrado pagina'>
+        <div className='pagina-naoEncontrado' onClick={voltar}>
 
-            <Cabecalho />
+            <section>
 
-            <Footer />
+            <h1>Ops!</h1>
+
+            <h3>Está página não foi encontrada.</h3>
+
+            </section>
+
+            <img src="/assets/imageErro/exis.webp" alt="" />
+
+            <p>Clique em qualquer lugar e volte a navegar</p>
 
         </div>
 
