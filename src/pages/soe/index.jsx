221ee7f07d1ac224dd26af61d0a26096089546f7
felipehtt <fs.oliveira0014@gmail.com>
2024-10-18 16:48:46 -0300

    s

--- src/pages/soe/index.jsx
+++ src/pages/soe/index.jsx
@@ -3,17 +3,30 @@ import './index.scss';
 import Cabecalho from '../../components/cabecalho';
 import Footer from '../../components/footer';
 
-export default function Soe(){
+export default function Soe() {
 
-    return(
+    return (
 
         <div className='pagina-soe pagina'>
 
-            <Cabecalho/>
+            <Cabecalho />
 
-                <div className=''>
+            <h1>S.O.E</h1>
             
-                </div>
+            <div className='bloco1'>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi saepe eum corporis id laboriosam at enim omnis dignissimos, atque velit eligendi deleniti nulla illo vitae delectus harum labore. Praesentium, velit.</p>
+
+                <img src="" alt="" />
+            </div>
+
+            <div className='bloco1'>
+
+                <img src="" alt="" />
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi saepe eum corporis id laboriosam at enim omnis dignissimos, atque velit eligendi deleniti nulla illo vitae delectus harum labore. Praesentium, velit.</p>
+                
+            </div>
 
             <Footer />
 
