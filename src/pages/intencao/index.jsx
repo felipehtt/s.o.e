01abc814bb8569d48865d82e0605d299e7ac374a
davi <daviforever17@gmail>
2024-10-20 15:09:33 -0300

    intention.

--- src/pages/intencao/index.jsx
+++ src/pages/intencao/index.jsx
@@ -5,7 +5,7 @@ import Footer from '../../components/footer';
 import axios from 'axios';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 import { useState } from 'react';
 
@@ -36,7 +36,7 @@ export default function Intencao() {
         const url = 'http://localhost:7000/intencao';
         let resp = await axios.post(url, paramCorpo);
 
-        alert('Pessoa adicionada na Intenções. Id: ' + resp.data.idIntencao);
+        alert('Intenção enviada com sucesso!');
 
     }
 
@@ -47,42 +47,46 @@ export default function Intencao() {
             <Cabecalho />
 
             <div className='top'>
-                <a className='icon' href="/"><FontAwesomeIcon icon={faArrowLeft} size='2x' /></a>
-
                 <h1>Preencha esse formulário para sua intenção.</h1>
             </div>
 
             <div className='form'>
                 <div>
-                    <label>Nome:</label>
-                    <input type='text' placeholder='Seu Nome' value={nome} onChange={e => setNome(e.target.value)} />
+                    <input type='text' placeholder='Nome Completo:' value={nome} onChange={e => setNome(e.target.value)} />
                 </div>
                 <div>
-                    <label>Telefone:</label>
-                    <input type='text' placeholder='(11)12345-1234' value={telefone} onChange={e => setTelefone(e.target.value)} />
+                    <input type='text' placeholder='Telefone:' value={telefone} onChange={e => setTelefone(e.target.value)} />
                 </div>
                 <div>
-                    <label>CEP:</label>
-                    <input type='text' placeholder='12345-123' value={cep} onChange={e => setCep(e.target.value)} />
+                    <input type='text' placeholder='Cep:' value={cep} onChange={e => setCep(e.target.value)} />
                 </div>
                 <div>
-                    <label>Data da Festa:</label>
-                    <input type='text' placeholder='05/05/2024' value={dataFesta} onChange={e => setDataFesta(e.target.value)} />
+                    <input type='date' placeholder='Data da Festa:' value={dataFesta} onChange={e => setDataFesta(e.target.value)} />
                 </div>
                 <div>
-                    <label>Tipo da Festa:</label>
-                    <input type='text' placeholder='Casamento' value={tipoFesta} onChange={e => setTipoFesta(e.target.value)} />
+                    <input type='text' placeholder='Tipo da Festa:' value={tipoFesta} onChange={e => setTipoFesta(e.target.value)} />
                 </div>
                 <div>
-                    <label>Tema da Festa:</label>
-                    <input type='text' placeholder='Heróis' value={temaFesta} onChange={e => setTemaFesta(e.target.value)} />
+                    <input type='text' placeholder='Tema da Festa:' value={temaFesta} onChange={e => setTemaFesta(e.target.value)} />
                 </div>
                 <div>
-                    <label>Data Intenção</label>
-                    <input type='text' value={dataIntencao} onChange={e => setDataIntencao(e.target.value)} />
+                    <input type='date' placeholder='Data da Intenção:' value={dataIntencao} onChange={e => setDataIntencao(e.target.value)} />
                 </div>
+
             </div>
-            <button onClick={salvarIntencao}> Enviar </button>
+
+            <div className='but'>
+
+                <button onClick={salvarIntencao}> ENVIAR </button>
+
+            </div>
+
+            <div className='cardW'>
+
+                <p> Após enviar, em breve entraremos em contato com você por Whatsapp:</p>
+                <a href=""><FontAwesomeIcon icon={faWhatsapp} size='3x'color='green'/></a>
+
+            </div>
 
 
             <Footer />
