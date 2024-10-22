4cbb3c44083f336ca552ad1ec88053d30535d5e7
felipehtt <fs.oliveira0014@gmail.com>
2024-10-22 15:49:12 -0300

    scss

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,24 @@
 import './index.scss';
+import { useState } from 'react';
+import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
+
+// const [nome, setNome] = useState('');
+// const [senha, setSenha] = useState('');
+
+// async function salvar() {
+//     const paramCorpo = {
+//         "nome": nome,
+//         "senha": senha
+//     }
+
+//     const url = 'http://localhost:7000/login';
+//     let resp = await axios.post(url, paramCorpo);
+
+
+// }
 
 export default function Login() {
 
