04d66e21a2ccda77530da205780a9375c6106b52
Guilherme Pianoski <ra52784596893@acaonsfatima.org.br>
2024-10-22 23:06:40 -0300

    Página de festas finalizada

--- src/pages/festas/jovens/index.jsx
+++ src/pages/festas/jovens/index.jsx
@@ -3,11 +3,11 @@ import './index.scss';
 import Cabecalho from '../../../components/cabecalho';
 import Footer from '../../../components/footer';
 
-export default function Adolescente() {
+export default function Jovens() {
 
     return (
 
-        <div className='pagina-adolescentes pagina'>
+        <div className='pagina-jovens pagina'>
 
             <Cabecalho />
 
