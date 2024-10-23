ab9f2d880feceb715f89f5b764f6c504d8b859c2
felipehtt <fs.oliveira0014@gmail.com>
2024-10-23 00:00:53 -0300

    dashboard 1

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,21 +1,73 @@
 import './index.scss';
-
+import { useState } from 'react';
 import CabecalhoDash from '../../components/cabecalho-dash';
 import Footer from '../../components/footer';
 
 export default function Dashboard() {
 
+    const [active, setActive] = useState('Festas');
+
+    const navItems = ["Festas", "Usuários", "Intenções", "Financeiro"];
+
     return (
 
-        <div className='pagina-dashbaord pagina'>
+        <div className='pagina-dashboard'>
 
             <CabecalhoDash />
 
-            <h1>Dashboard</h1>
+            <h1>Seja bem-vindo ao Dashboard do Administrador!</h1>
+            
+            <div className="dash-container">
 
-            <a href="">Cadastrar Evento</a>
+                
 
-            <a href="">Consultar</a>
+                <div className="sidebar">
+
+                    <div className="user-profile">
+
+                        <img width='60px' src="assets/images/logo-soe.png" alt="Logo" />
+
+                        <div>
+                            <p className="username">Zuleica</p>
+
+                            <p className="user-role">Admin</p>
+                        </div>
+
+                    </div>
+
+                    <ul className="nav-list">
+                        {navItems.map((item) => (
+                            <li
+                                key={item}
+                                className={`nav-item ${active === item ? "active" : ""}`}
+                                onClick={() => setActive(item)}
+                            >
+                                {item}
+                            </li>
+                        ))}
+                    </ul>
+
+                    <div className='but'>
+
+                        <button>Log Out</button>
+
+                    </div>
+
+                </div>
+
+                <div className="dashboard-content">
+
+                    <h1>{active}</h1>
+
+                    <p>
+                        {active === "Festas"
+                            ? "Cadastre e consulte as festas que fará neste mês."
+                            : `Veja aqui a seção: ${active}.`}
+                    </p>
+
+                </div>
+
+            </div>
 
             <Footer />
 
