4147c7511baedcb5fb0d014512802a4e643da3ef
matheustake <matheussouzamsantos@gmail.com>
2024-10-25 17:35:57 -0300

    festas jovens

--- src/pages/festas/jovens/index.jsx
+++ src/pages/festas/jovens/index.jsx
@@ -24,36 +24,39 @@ export default function Jovens() {
 
             <div className='bloco1'>
 
-                <div className='textos'>
+               
+           
+                <img src="https://curitifestas.com.br/wp-content/uploads/2023/11/Decoracao-15-anos-Beatriz.jpeg?v=1705844675" alt="" />
+            <div className='textos'>
 
                 <h2> festas de 15 anos   </h2>
-                <p>sdajkbfdasjhibdiu</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptatibus odit incidunt et illum dolores blanditiis sit illo quas fuga aut, optio quae. Consectetur incidunt in, neque quia sapiente quam.</p>
+                </div> 
                 </div>
-           
-                <img src="https://i.pinimg.com/736x/65/68/57/6568570b99ac76306abf1d98fbe8982c.jpg" alt="" />
-            </div>
 
             <div className='bloco2'>
 
         <div className='textos'>
 
-        <h2> festas de 15 anos   </h2>
-        <p>sdajkbfdasjhibdiu</p>
+        <h2>Halloween   </h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor earum ipsum voluptatibus magni eum, reprehenderit mollitia explicabo! Molestias doloribus perferendis dolorem ipsam enim quis cumque odit blanditiis, velit commodi excepturi?</p>
         </div>
 
-        <img src="https://i.pinimg.com/736x/65/68/57/6568570b99ac76306abf1d98fbe8982c.jpg" alt="" />
+        <img src="https://cdn.shopify.com/s/files/1/0533/3906/2441/files/3_77066389-f07c-4175-85b4-28b26024fdc9.jpg?v=1664437871" alt="" />
         </div>
            
 
         <div className='bloco3'>
 
+        
+
+        <img src="https://i.pinimg.com/564x/18/be/65/18be651e5492fe478810d6a0ed08fbb0.jpg" alt="" />
         <div className='textos'>
 
-        <h2> festas de 15 anos   </h2>
-        <p>sdajkbfdasjhibdiu</p>
+        <h2>Neon    </h2>
+        
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque praesentium, molestiae quasi tenetur dolor, dolorum quidem impedit accusantium exercitationem non rem distinctio consectetur iure autem tempora quaerat commodi itaque temporibus.</p>
         </div>
-
-        <img src="https://i.pinimg.com/736x/65/68/57/6568570b99ac76306abf1d98fbe8982c.jpg" alt="" />
         </div>
            
            
