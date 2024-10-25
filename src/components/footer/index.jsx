24e52737294c78f7cba1fded44e066455abd4c34
felipehtt <fs.oliveira0014@gmail.com>
2024-10-25 15:31:06 -0300

    footer.

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,37 +1,49 @@
+import React from 'react';
 import './index.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircle, faCopyright } from '@fortawesome/free-solid-svg-icons';
-import { faInstagram, faTiktok, faFacebookF } from '@fortawesome/free-brands-svg-icons';
-import { Link } from 'react-router-dom';
+import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedin, FaWhatsapp } from 'react-icons/fa';
 
 export default function Footer() {
-
     return (
+        <footer className="footer">
+            <div className="footer-content">
+                <div className="contato">
+                    <h2>Contate-nos</h2>
+                    <div className="social-links">
+                        <a href="#" className="social-link"><FaWhatsapp fontSize='140%'/></a>
+                        <p>soefestas@gmail.com</p>
+                     </div>
+                </div>
 
-        <div className='comp-footer'>
-
-            <div className='nav'>
-
-                <section>
-
-                    <Link> <FontAwesomeIcon icon={faInstagram} size='2x'/> </Link>
-                    <Link> <FontAwesomeIcon icon={faFacebookF} size='2x'/> </Link>
-                    <Link> <FontAwesomeIcon icon={faTiktok} size='2x'/> </Link>
-
-                </section>
-
-                <p> Special  <FontAwesomeIcon className='c' icon={faCircle}/>   Organization <FontAwesomeIcon className='c' icon={faCircle} />  Events </p>
-
-                <p className='m'>Terms of Use  -  Privacy  Police</p>
-
-                <p className='n'> <FontAwesomeIcon icon={faCopyright} /> 2024 Clarity Money Legacy</p>
+                <div className="social">
+                    <h2>Siga-nos</h2>
+                    <div className="social-links">
+                        <a href="#" className="social-link"><FaFacebookF fontSize='140%' /></a>
+                        <a href="#" className="social-link"><FaTwitter fontSize='140%' /></a>
+                        <a href="#" className="social-link"><FaInstagram fontSize='140%' /></a>
+                        <a href="#" className="social-link"><FaLinkedin fontSize='140%' /></a>
+                    </div>
+                </div>
             </div>
 
-        </div>
+            <div className="footer-baixo">
+                <nav className="footer-nav">
+                    <a href="/soe">Sobre nós</a>
+                    <a href="/festas">Festas</a>
+                    <a href="#">Contato</a>
+                    <a href="#">Política de Privacidade</a>
+                </nav>
 
+                <div className='end'>
+
+                    <p>&copy; {new Date().getFullYear()} Soe Site.</p>
+
+                    <img src="/assets/images/logo-soe.png" alt="" />
+
+                    <p>Direitos reservados.</p>
+
+                </div>
+            </div>
+        </footer>
     );
-
-}
-
-
+};
 
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,7 +2,7 @@ import './index.scss';
 
 import Cabecalho from '../../components/cabecalho';
 import Slider from '../../components/Slider';
-import Footer from '../../components/footer';
+import Footer from '../../components/f2';
 
 export default function Home() {
 
@@ -12,47 +12,44 @@ export default function Home() {
 
       <Cabecalho />
 
-      <Slider img1="'https://wallpapercave.com/wp/wp2848100.jpg'"/>
-
       <main>
 
+        <Slider img1="'https://wallpapercave.com/wp/wp2848100.jpg'" />
 
-          <div className='top'>
+        <div className='top'>
 
-            <p>O melhor da festa é esperar por ela. Há 15 anos no mercado de festas e eventos !</p>
+          <p>O melhor da festa é esperar por ela.</p>
 
-          </div>
-          
-          <div className='images'> 
+        </div>
 
-            <div className='conj'>
+        <div className='images'>
 
-              <img src="https://th.bing.com/th/id/OIP.e2L4oAqaiCuGoo-1AvkSdQHaE8?rs=1&pid=ImgDetMain" alt="" />
-              <p>Festas Infantis</p>
-              
-            </div>
+          <div className='conj'>
 
-            <div className='conj'>
-
-              <img src="https://casarpontocom-inspiracoes.s3.sa-east-1.amazonaws.com/wp-content/uploads/2023/02/festa-de-casamento-Foto-Freepik-6.jpg" alt="" />
-              <p>Festas Jovens</p>
-
-            </div>
-
-            <div className='conj'>
-
-              <img src="https://lh7-us.googleusercontent.com/7mf8WmPAFQsRVxxVOXwU4t86HaT-Xk3iucLBXn8K3vcB1-3cVd_M1F--WJZsTQMSHWMSipZNqAz4LRM8gGpavDDTLSpi1ukI0RbNijv0pq1lhuE9xB_NJmPG9WjHmvxxZpebrgFOQJlkl2lx_ZeZFVU" alt="" />
-              <p>Festas Adultos</p>
-
-            </div>
+            <img src="https://th.bing.com/th/id/OIP.e2L4oAqaiCuGoo-1AvkSdQHaE8?rs=1&pid=ImgDetMain" alt="" />
+            <p>Festas Infantis</p>
 
           </div>
 
-          <div className='bottom'>
+          <div className='conj'>
 
-            <p className='bt'>Aproveite com seus amigos e familiares...</p>
+            <img src="https://casarpontocom-inspiracoes.s3.sa-east-1.amazonaws.com/wp-content/uploads/2023/02/festa-de-casamento-Foto-Freepik-6.jpg" alt="" />
+            <p>Festas Jovens</p>
 
           </div>
+
+          <div className='conj'>
+
+            <img src="https://lh7-us.googleusercontent.com/7mf8WmPAFQsRVxxVOXwU4t86HaT-Xk3iucLBXn8K3vcB1-3cVd_M1F--WJZsTQMSHWMSipZNqAz4LRM8gGpavDDTLSpi1ukI0RbNijv0pq1lhuE9xB_NJmPG9WjHmvxxZpebrgFOQJlkl2lx_ZeZFVU" alt="" />
+            <p>Festas Adultos</p>
+
+          </div>
+
+        </div>
+
+        <div className='bottom'>
+          <p>Há 15 anos no mercado de festas e eventos !</p>
+        </div>
 
       </main>
 
