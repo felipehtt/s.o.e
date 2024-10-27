88e4fac1c3e08ca626bd9e62a110f56460ea59ca
Matheus Souza <matheussouzamsantos@gmail.com>
2024-10-27 04:58:22 -0300

    foi?

--- src/pages/festas/adulto/index.jsx
+++ src/pages/festas/adulto/index.jsx
@@ -12,6 +12,8 @@ export default function Adulto(){
 
 <Cabecalho />
 
+<Fly/>
+
 <div className='static'>
 
     <img src='https://i.pinimg.com/originals/6a/d8/5b/6ad85bf9a8d499a9c21a9432888d256c.jpg' alt='' />
@@ -30,6 +32,10 @@ export default function Adulto(){
 
             <p>Essa é uma festa em que cada detalhe irradia amor e elegância, perfeita para celebrar um casamento inesquecível. Transforme o grande dia com nossas decorações exclusivas, pensadas para criar momentos e memórias que durarão para sempre na história do noivo e da noiva.</p>
 
+            <a href='/intencao' className='but' >
+        <Link to='/intencao' className='but2'>Gostou? Faça sua Intenção</Link>
+        </a>
+        
         </div>
 
         <img src='https://media.discordapp.net/attachments/1253785078484434957/1299805998176927835/FB_IMG_1729968235628.jpg?ex=671e8a2b&is=671d38ab&hm=982245e9ca353c566448a02a17d50fe877090f1b9ba30d562b124d93866c5096&=&format=webp&width=587&height=498' alt='Festa de Casamento' />
@@ -45,9 +51,13 @@ export default function Adulto(){
             <h3>Flores</h3>
 
             <p>Essa é uma festa em que cada detalhe exalta a beleza das flores, perfeita para criar um evento inesquecível. Transforme a celebração com nossas decorações exclusivas, inspiradas no tema floral, pensadas para proporcionar momentos encantadores e memórias que florescerão para sempre na vida do aniversariante.</p>
+        
+    <a href='/intencao' className='but' >
+        
+        <Link to='/intencao' className='but2'>Gostou? Faça sua Intenção</Link>
+        
+    </a>
 
-<Link to='/intencao' className='but2'>Gostou? Faça sua Intenção</Link>
-        </a>
         </div>
 
     </div>
@@ -60,6 +70,9 @@ export default function Adulto(){
 
             <p>Essa festa é uma verdadeira celebração de sentimentos e conexões. Com decorações que destacam mensagens personalizadas, cada espaço do evento será uma oportunidade de refletir sobre as boas memórias. É o momento perfeito para criar uma atmosfera especial, onde todos podem se sentir inspirados e apreciados.</p>
 
+            <a href='/intencao' className='but' >
+        <Link to='/intencao' className='but2'>Gostou? Faça sua Intenção</Link>
+        </a>
         </div>
 
         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJEFemScXUlUUNJWPRd1nOG3fs7Uwcd1c6Wg&s' alt='Festa de Anos 50' />
