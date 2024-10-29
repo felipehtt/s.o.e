3896917ca53d6eaccb579b7aff7a539a4646d5ca
davi <daviforever17@gmail>
2024-10-29 00:46:10 -0300

    dash

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -100,27 +100,26 @@ export default function Dashboard() {
 
             </div>
 
-            <div className="dash-container">
+            <div className='dash-container'>
 
-                <div className="sidebar">
+                <div className='sidebar'>
 
-                    <div className="user-profile">
+                    <div className='user-profile'>
 
                         <img width='60px' src="assets/images/logo-soe.png" alt="Logo" />
 
                         <div>
-                            <p className="username">{userName}</p>
+                            <p className='username'>{userName}</p>
 
-                            <p className="user-role">Admin</p>
-                        </div>
-
+                            <p className='user-role'>Admin</p>
+                        </div>'
                     </div>
 
-                    <ul className="nav-list">
+                    <ul className='nav-list'>
                         {navItems.map((item) => (
                             <li
                                 key={item}
-                                className={`nav-item ${active === item ? "active" : ""}`}
+                                className={`nav-item ${active === item ? 'active' : ''}`}
                                 onClick={() => setActive(item)}
                             >
                                 {item}
@@ -136,12 +135,13 @@ export default function Dashboard() {
 
                 </div>
 
-                <div className="dashboard-content">
+                <div className='dashboard-content'>
 
                     {active == 'Intenções' &&
 
                         <div className='nav-intention'>
 
+<<<<<<< HEAD
                             <button onClick={buscarIntencoes} className='get-intention'>Buscar intenções</button>
 
                             <table>
@@ -187,12 +187,19 @@ export default function Dashboard() {
                         </div>
 
                     }
+=======
+                    <p>
+                        {active === 'Festas'
+                            ? 'Cadastre e consulte as festas que fará neste mês.'
+                            : `Veja aqui a seção: ${active}.`}
+                    </p>
+>>>>>>> 07f729029b31968f7a5a4be18120d4353757284b
 
                 </div>
 
             </div>
 
-            <Footer />
+            {/* <Footer /> */}
 
         </div>
 
